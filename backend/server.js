3e139e6be3cf87cda9420f39d1a43b6806c9daf9
Karthikha PR <karthikhapr@gmail.com>
2026-03-04 09:31:46 +0530

    fix paths

--- backend/server.js
+++ backend/server.js
@@ -54,8 +54,5 @@ app.post("/groups", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
-app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
